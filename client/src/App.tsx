bdf915542eb922089744f5b04d88947ffab5deba
mihail.gaberov <mihail.gaberov@ysura.com>
2022-09-02 11:49:50 +0200

    ✨Introduced Apollo client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,63 @@
-import { Subscriber } from "./components/Subscriber";
-import "./App.scss";
-import { Publisher } from "./components/Publisher";
 import React from "react";
+import { Subscriber } from "./components/Subscriber";
+import { Publisher } from "./components/Publisher";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloProvider } from "react-apollo";
+import { split } from "apollo-link";
+import { HttpLink } from "apollo-link-http";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
+
+import "./App.scss";
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000",
+});
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+  },
+});
+
+const link = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="app">
-      <section className="publishers">
-        {["⛅", "🤾‍♀️", "🎶"].map((channel) => (
-          <Publisher
-            icon={channel}
-            publishCallback={() => console.log(">>> publish")}
-          />
-        ))}
-      </section>
-      <section className="subscribers">
-        {[1, 3, 4].map((n) => (
-          <Subscriber data={n} />
-        ))}
-      </section>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="app">
+        <section className="publishers">
+          {["⛅", "🤾‍♀️", "🎶"].map((channel) => (
+            <Publisher
+              icon={channel}
+              publishCallback={() => console.log(">>> publish")}
+            />
+          ))}
+        </section>
+        <section className="subscribers">
+          {[1, 3, 4].map((n) => (
+            <Subscriber data={n} />
+          ))}
+        </section>
+      </div>
+    </ApolloProvider>
   );
 }
 
--- client/src/components/Publisher/Publisher.tsx
+++ client/src/components/Publisher/Publisher.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, MouseEventHandler } from "react";
+import React, { FunctionComponent, MouseEventHandler } from "react";
 
 import "./Publisher.scss";
 
