93de5bef005a9fbb22f526b38cc21b1013c319e0
mihail.gaberov <mihail.gaberov@ysura.com>
2022-09-02 12:22:17 +0200

    ✨Add subscriber buttons

--- client/src/components/Subscriber/Subscriber.tsx
+++ client/src/components/Subscriber/Subscriber.tsx
@@ -13,8 +13,23 @@ export const Subscriber: FunctionComponent<SubscriberProps> = ({
       <div className="side front">
         <h3>Subscriber #{subscriberNo}</h3>
       </div>
-
-      <div className="side back"></div>
+      <div className="side back">
+        <button onClick={() => console.log(">>> Subscribe for weather")}>
+          <sup>Subscribe for</sup>
+          <br />
+          Weather
+        </button>
+        <button onClick={() => console.log(">>> Subscribe for sport")}>
+          <sup>Subscribe for</sup>
+          <br />
+          Sport
+        </button>
+        <button disabled onClick={() => console.log(">>> Subscribe for music")}>
+          <sup>Subscribe for</sup>
+          <br />
+          Music
+        </button>
+      </div>
     </div>
   );
 };
