6e23e24e8d19754b804b28555dca1e24c5aaee1b
Mihail Gaberov <mihail.gaberov@gmail.com>
2022-09-05 13:59:29 +0200

    :rocket: Trying to deploy the server on Heroku

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,4 @@
-import { ApolloServer } from "apollo-server";
+/* import { ApolloServer } from "apollo-server";
 
 import typeDefs from "./schema";
 import resolvers from "./resolvers";
@@ -15,3 +15,38 @@ server.applyMiddleware();
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
+ */
+import { ApolloServer } from "apollo-server-express";
+import {
+  ApolloServerPluginDrainHttpServer,
+  ApolloServerPluginLandingPageLocalDefault,
+} from "apollo-server-core";
+import express from "express";
+import http from "http";
+import typeDefs from "./schema";
+import resolvers from "./resolvers";
+
+async function startApolloServer(typeDefs: any, resolvers: any) {
+  const app = express();
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    csrfPrevention: true,
+    cache: "bounded",
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+    ],
+  });
+  await server.start();
+  server.applyMiddleware({
+    app,
+    path: "https://redis-pub-sub-visualized.vercel.app/",
+    cors: false,
+  });
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: 4000 }, resolve)
+  );
+  console.log(`🚀 Server ready at ${server.graphqlPath}`);
+}
