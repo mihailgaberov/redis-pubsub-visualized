87ec1087b3d96766be3a3f3e7453b6c5c9d33535
Mihail Gaberov <mihail.gaberov@gmail.com>
2022-09-05 15:56:17 +0200

    :rocket: Trying to deploy the server on Heroku

--- server/utils/redis.ts
+++ server/utils/redis.ts
@@ -7,7 +7,7 @@ const redis = new Redis({
   port: Number(process.env.REDIS_PORT),
   host: process.env.REDIS_HOST,
   username: process.env.REDIS_USER,
-  password: process.env.REDIS_PASS,
+  password: process.env.REDIS_PASSWORD,
   db: 0,
 });
 
