9cce9141de8528121f5385edbd8c918ccff0af5b
Mihail Gaberov <mihail.gaberov@gmail.com>
2022-09-05 13:10:55 +0200

    :rocket: Enable graphql playground on production

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,12 @@ import { ApolloServer } from "apollo-server";
 import typeDefs from "./schema";
 import resolvers from "./resolvers";
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: true,
+  introspection: true,
+});
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
