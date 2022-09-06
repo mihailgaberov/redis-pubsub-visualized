3d2a937062f946d33a7351731394831b843c6333
mihail.gaberov <mihail.gaberov@ysura.com>
2022-09-06 12:16:38 +0200

    :rocket: Deploy back end to Heroku

--- server/pubsub.ts
+++ server/pubsub.ts
@@ -1,5 +1,22 @@
 import { RedisPubSub } from "graphql-redis-subscriptions";
+import Redis from "ioredis";
+import dotenv from "dotenv";
 
-const pubsub = new RedisPubSub();
+dotenv.config();
+
+const REDISCLOUD_URL = process.env.REDISCLOUD_URL || "redis://127.0.0.1:6379";
+
+const options = {
+  retryStrategy: (times: number) => {
+    // reconnect after
+    return Math.min(times * 50, 2000);
+  },
+};
+
+const pubsub = new RedisPubSub({
+  publisher: new Redis(REDISCLOUD_URL, options),
+  subscriber: new Redis(REDISCLOUD_URL, options),
+});
+// const pubsub = new RedisPubSub();
 
 export default pubsub;
--- server/utils/redis.ts
+++ server/utils/redis.ts
@@ -1,37 +1,35 @@
-// import Redis, { RedisKey } from "ioredis";
-import { createClient } from "redis";
+import Redis from "ioredis";
+// import { createClient } from "redis";
 import dotenv from "dotenv";
-
 dotenv.config();
-// const redis = new Redis({
-//   port: Number(process.env.REDIS_PORT),
-//   host: process.env.REDIS_HOST,
-//   username: process.env.REDIS_USER,
-//   password: process.env.REDIS_PASSWORD,
-//   db: 0,
-// });
+
 const username: string = process.env.REDIS_USER ?? "";
 const password: string = process.env.REDIS_PASSWORD ?? "";
 const host: string = process.env.REDIS_HOST ?? "";
 const port: string = process.env.REDIS_PORT ?? "";
+
+const client = new Redis({
+  port: Number(process.env.REDIS_PORT),
+  host: process.env.REDIS_HOST,
+  username: process.env.REDIS_USER,
+  password: process.env.REDIS_PASSWORD,
+  db: 0,
+});
 
 console.log(">>> host: ", host);
 console.log(">>> port: ", port);
 console.log(">>> username: ", username);
 console.log(">>> password: ", password);
 
-const client = createClient({
-  url: `redis://${username}:${password}@${host}:${port}`,
-});
+// const client = createClient({
+//   url: `redis://${username}:${password}@${host}:${port}`,
+// });
 
-client.on("error", (err) => console.log("Redis Client Error", err));
+// client.on("error", (err) => console.log("Redis Client Error", err));
 
-(async () => {
-  await client.connect();
-})();
-// await client.set("key", "value");
-// const value = await client.get("key");
-
+// (async () => {
+//   await client.connect();
+// })();
 export const get = async (key: any) => {
   try {
     const data: any = await client.get(key);
