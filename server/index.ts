efe1b6d94c29224b09bc04a8c9088bdd9e42661f
mihail.gaberov <mihail.gaberov@ysura.com>
2022-09-07 17:26:53 +0200

    🎉 Fix the server - add websocket server needed by the subscriptions

--- server/index.ts
+++ server/index.ts
@@ -1,43 +1,64 @@
 import { ApolloServer } from "apollo-server-express";
+import { createServer } from "http";
+import express from "express";
 import {
   ApolloServerPluginDrainHttpServer,
   ApolloServerPluginLandingPageLocalDefault,
 } from "apollo-server-core";
-import express from "express";
-import http from "http";
-
-import typeDefs from "./schema";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { WebSocketServer } from "ws";
+import { useServer } from "graphql-ws/lib/use/ws";
 import resolvers from "./resolvers";
+import typeDefs from "./schema";
 
 (async () => {
-  const app = express();
+  // Create the schema, which will be used separately by ApolloServer and
+  // the WebSocket server.
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-  const httpServer = http.createServer(app);
+  // Create an Express app and HTTP server; we will attach both the WebSocket
+  // server and the ApolloServer to this HTTP server.
+  const app = express();
+  const httpServer = createServer(app);
+
+  // Create our WebSocket server using the HTTP server we just set up.
+  const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: "/graphql",
+  });
+  // Save the returned server's info so we can shutdown this server later
+  const serverCleanup = useServer({ schema }, wsServer);
+
+  // Set up ApolloServer.
   const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    introspection: true,
+    schema,
     csrfPrevention: true,
     cache: "bounded",
     plugins: [
+      // Proper shutdown for the HTTP server.
       ApolloServerPluginDrainHttpServer({ httpServer }),
+
+      // Proper shutdown for the WebSocket server.
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await serverCleanup.dispose();
+            },
+          };
+        },
+      },
       ApolloServerPluginLandingPageLocalDefault({ embed: true }),
     ],
   });
-
   await server.start();
+  server.applyMiddleware({ app });
 
-  const corsOptions = {
-    origin: "http://localhost:5173",
-  };
-
-  server.applyMiddleware({
-    app,
-    path: "/graphql",
-    cors: corsOptions,
+  const PORT = 4000;
+  // Now that our HTTP server is fully set up, we can listen to it.
+  httpServer.listen(PORT, () => {
+    console.log(
+      `Server is now running on http://localhost:${PORT}${server.graphqlPath}`
+    );
   });
-  await new Promise<void>((resolve) =>
-    httpServer.listen({ port: 4000 }, resolve)
-  );
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
 })();
