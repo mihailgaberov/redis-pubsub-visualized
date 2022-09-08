f825de8d7324e7d8c530e5fa0848b5582e86463c
mihail.gaberov <mihail.gaberov@ysura.com>
2022-09-08 15:28:01 +0200

    :bulb: Remove comments

--- server/index.ts
+++ server/index.ts
@@ -12,33 +12,23 @@ import resolvers from "./resolvers";
 import typeDefs from "./schema";
 
 (async () => {
-  // Create the schema, which will be used separately by ApolloServer and
-  // the WebSocket server.
   const schema = makeExecutableSchema({ typeDefs, resolvers });
-
-  // Create an Express app and HTTP server; we will attach both the WebSocket
-  // server and the ApolloServer to this HTTP server.
   const app = express();
   const httpServer = createServer(app);
 
-  // Create our WebSocket server using the HTTP server we just set up.
   const wsServer = new WebSocketServer({
     server: httpServer,
     path: "/graphql",
   });
-  // Save the returned server's info so we can shutdown this server later
+
   const serverCleanup = useServer({ schema }, wsServer);
 
-  // Set up ApolloServer.
   const server = new ApolloServer({
     schema,
     csrfPrevention: true,
     cache: "bounded",
     plugins: [
-      // Proper shutdown for the HTTP server.
       ApolloServerPluginDrainHttpServer({ httpServer }),
-
-      // Proper shutdown for the WebSocket server.
       {
         async serverWillStart() {
           return {
@@ -55,7 +45,6 @@ import typeDefs from "./schema";
   server.applyMiddleware({ app });
 
   const PORT = 4000;
-  // Now that our HTTP server is fully set up, we can listen to it.
   httpServer.listen(PORT, () => {
     console.log(
       `Server is now running on http://localhost:${PORT}${server.graphqlPath}`
