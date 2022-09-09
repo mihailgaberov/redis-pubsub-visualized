5c6d2ca99df58ac09a7273b220851b1de67bfd95
mihail.gaberov <mihail.gaberov@ysura.com>
2022-09-09 13:00:50 +0200

    :rocket: Deploying to render

--- server/utils/redis.ts
+++ server/utils/redis.ts
@@ -2,10 +2,10 @@ import dotenv from "dotenv";
 import Redis from "ioredis";
 dotenv.config();
 
-const username: string = process.env.REDIS_USER ?? "";
-const password: string = process.env.REDIS_PASSWORD ?? "";
-const host: string = process.env.REDIS_HOST ?? "";
-const port: number = Number(process.env.REDIS_PORT) ?? 6376;
+// const username: string = process.env.REDIS_USER ?? "";
+// const password: string = process.env.REDIS_PASSWORD ?? "";
+// const host: string = process.env.REDIS_HOST ?? "";
+// const port: number = Number(process.env.REDIS_PORT) ?? 6376;
 
 // const client = new Redis({
 //   port: port,
