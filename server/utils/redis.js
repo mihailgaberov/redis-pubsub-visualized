0b04a1f1fc599e8cd4461c4120cf29a83a5fe303
mihail.gaberov <mihail.gaberov@ysura.com>
2022-09-22 14:48:18 +0200

    🚀 Trying deploying on Render

--- server/utils/redis.js
+++ server/utils/redis.js
@@ -2,20 +2,23 @@ const Redis = require("ioredis");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const username = process.env.REDIS_USER ?? "";
-const password = process.env.REDIS_PASSWORD ?? "";
+// const username = process.env.REDIS_USER ?? "";
+// const password = process.env.REDIS_PASSWORD ?? "";
 const host = process.env.REDIS_HOST ?? "";
 const port = Number(process.env.REDIS_PORT) ?? 6376;
 
-const client = new Redis({
-  port: port,
-  host: host,
-  username: username,
-  password: password,
-  connectionName: "Migelito",
-});
+// const client = new Redis({
+//   port: port,
+//   host: host,
+//   username: username,
+//   password: password,
+//   connectionName: "Migelito",
+// });
 // const { REDISCLOUD_URL } = process.env;
-// const client = new Redis(REDISCLOUD_URL);
+const client = new Redis({
+  host,
+  port,
+});
 if (client) {
   console.log("Connected to Redis! 🚀");
 
