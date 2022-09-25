023379a26b754cfbee893a395173cdfd161c3d15
Mihail Gaberov <mihail.gaberov@gmail.com>
2022-09-25 10:55:41 +0200

    :rocket: Trying to run redis server as daemon

--- server/utils/redis.js
+++ server/utils/redis.js
@@ -2,32 +2,32 @@ const Redis = require("ioredis");
 const dotenv = require("dotenv");
 dotenv.config();
 
-console.log(">>> env: ", process.env);
+console.log(">>> redis.js env: ", process.env);
 
-// const username = process.env.REDIS_USER ?? "";
-// const password = process.env.REDIS_PASSWORD ?? "";
+const username = process.env.REDIS_USER ?? "";
+const password = process.env.REDIS_PASSWORD ?? "";
 const host = process.env.REDIS_HOST ?? "";
 const port = Number(process.env.REDIS_PORT) ?? 6376;
 
-// const client = new Redis({
-//   port: port,
-//   host: host,
-//   username: username,
-//   password: password,
-//   connectionName: "Migelito",
-// });
-// const { REDISCLOUD_URL } = process.env;
 const client = new Redis({
-  host,
-  port,
+  port: port,
+  host: host,
+  username: username,
+  password: password,
+  connectionName: "Migelito",
 });
+// const { REDISCLOUD_URL } = process.env;
+// const client = new Redis({
+//   host,
+//   port,
+// });
 if (client) {
   console.log("Connected to Redis! 🚀");
 
   client.set("animal", "cat");
 
   client.get("animal").then((result) => {
-    console.log(`Result for key animal: ${result}`);
+    console.log(`🎉🎉🎉 Result for key animal: ${result}`);
   });
 
   client.del("animal");
